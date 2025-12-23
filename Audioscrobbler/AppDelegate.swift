62aa2d2b1f343251e9f97a7498c7eb36ff0af753
Toni Oriol <tonioriol@gmail.com>
2025-12-23 14:40:49 +0100

    fix: improve popover stability in fullscreen mode

--- Audioscrobbler/AppDelegate.swift
+++ Audioscrobbler/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSPopoverDelegate {
     var statusBarItem: NSStatusItem!
     var launchAtLoginItem: NSMenuItem!
     var contextMenu: NSMenu!
+    var eventMonitor: Any?
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         contextMenu = NSMenu()
@@ -35,8 +36,8 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSPopoverDelegate {
 
         let contentView = MainContentView()
         let popover = NSPopover()
-        popover.contentSize = NSSize(width: 400, height: 400)
-        popover.behavior = .transient
+        popover.contentSize = NSSize(width: 400, height: 700)
+        popover.behavior = .applicationDefined
         popover.contentViewController = NSHostingController(rootView: contentView)
         popover.delegate = self
         self.popover = popover
@@ -80,22 +81,48 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSPopoverDelegate {
             let event = NSApp.currentEvent!
             if event.type == NSEvent.EventType.rightMouseUp {
                 if self.popover.isShown {
-                    self.popover.performClose(sender)
+                    closePopover()
                 }
 
                 NSMenu.popUpContextMenu(contextMenu, with: event, for: self.statusBarItem.button!)
             } else {
                 if self.popover.isShown {
-                    self.popover.performClose(sender)
+                    closePopover()
                 } else {
                     self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
-                    popover.contentViewController?.view.window?.makeKey()
+                    if let popoverWindow = popover.contentViewController?.view.window {
+                        popoverWindow.level = .floating
+                        popoverWindow.collectionBehavior = .fullScreenAuxiliary
+                        popoverWindow.makeKey()
+                    }
+                    startMonitoring()
                 }
             }
         }
     }
+    
+    func closePopover() {
+        self.popover.performClose(nil)
+        stopMonitoring()
+    }
+    
+    func startMonitoring() {
+        eventMonitor = NSEvent.addGlobalMonitorForEvents(matching: [.leftMouseDown, .rightMouseDown]) { [weak self] event in
+            if let strongSelf = self, strongSelf.popover.isShown {
+                strongSelf.closePopover()
+            }
+        }
+    }
+    
+    func stopMonitoring() {
+        if let monitor = eventMonitor {
+            NSEvent.removeMonitor(monitor)
+            eventMonitor = nil
+        }
+    }
 
     func popoverWillClose(_ notification: Notification) {
+        stopMonitoring()
         NotificationCenter.default.post(name: NSNotification.Name("AudioscrobblerWillHide"), object: nil)
     }
 
