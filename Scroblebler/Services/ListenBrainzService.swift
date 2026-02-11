d59d7c738ede91f26ed30b0376b3d2100f66a3c6
Toni Oriol <tonioriol@gmail.com>
2026-02-11 19:10:23 +0100

    fix(listenbrainz): use track/album fallback search types

--- Scroblebler/Services/ListenBrainzService.swift
+++ Scroblebler/Services/ListenBrainzService.swift
@@ -3,18 +3,18 @@ import Foundation
 /// ListenBrainz service implementation
 class ListenBrainzService: Service {
     let client: ScrobbleClient
-    
+
     init(client: ScrobbleClient) {
         self.client = client
     }
-    
+
     func buildURLs(for track: Track) -> (artistURL: URL, albumURL: URL, trackURL: URL) {
         let encodedArtist = track.artist.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? track.artist
         let encodedAlbum = track.album.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? track.album
         let encodedTrack = track.name.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? track.name
-        
+
         let serviceInfo = track.serviceInfo[.listenbrainz]
-        
+
         // Artist URL: use artist MBID if available, otherwise search
         let artistURL: URL
         if let artistMbid = serviceInfo?.artistMbid {
@@ -22,33 +22,33 @@ class ListenBrainzService: Service {
         } else {
             artistURL = URL(string: "https://listenbrainz.org/search/?search_term=\(encodedArtist)&search_type=artist")!
         }
-        
+
         // Album URL: use release MBID if available, otherwise search
         let albumURL: URL
         if let releaseMbid = serviceInfo?.releaseMbid {
             albumURL = URL(string: "https://listenbrainz.org/release/\(releaseMbid)/")!
         } else {
-            albumURL = URL(string: "https://listenbrainz.org/search/?search_term=\(encodedArtist)%20\(encodedAlbum)&search_type=release")!
+            albumURL = URL(string: "https://listenbrainz.org/search/?search_term=\(encodedArtist)%20\(encodedAlbum)&search_type=album")!
         }
-        
+
         // Track URL: use recording MBID if available, otherwise search
         let trackURL: URL
         if let recordingMbid = serviceInfo?.id {
             trackURL = URL(string: "https://listenbrainz.org/track/\(recordingMbid)/")!
         } else {
-            trackURL = URL(string: "https://listenbrainz.org/search/?search_term=\(encodedArtist)%20\(encodedTrack)&search_type=recording")!
+            trackURL = URL(string: "https://listenbrainz.org/search/?search_term=\(encodedArtist)%20\(encodedTrack)&search_type=track")!
         }
-        
+
         return (artistURL: artistURL, albumURL: albumURL, trackURL: trackURL)
     }
-    
+
     func enrichTrack(_ track: Track) async -> Track {
         // If track already has all MBIDs, no need to enrich
         let existing = track.serviceInfo[.listenbrainz]
         if existing?.id != nil && existing?.artistMbid != nil && existing?.releaseMbid != nil {
             return track
         }
-        
+
         // Lookup MBIDs via MBID Mapper API
         guard let lbClient = client as? ListenBrainzClient,
               let mbids = try? await lbClient.lookupMBIDsForTrack(
@@ -59,7 +59,7 @@ class ListenBrainzService: Service {
               let recordingMbid = mbids.recordingMbid else {
             return track
         }
-        
+
         // Update track with all MBIDs (recording, artist, release)
         var enrichedTrack = track
         enrichedTrack.serviceInfo[.listenbrainz] = ServiceTrackData.listenbrainzWithMbids(
@@ -68,7 +68,7 @@ class ListenBrainzService: Service {
             releaseMbid: mbids.releaseMbid,
             timestamp: track.timestamp
         )
-        
+
         Logger.debug("ListenBrainz enriched '\(track.name)' with MBIDs - recording: \(recordingMbid), artist: \(mbids.artistMbid ?? "none"), release: \(mbids.releaseMbid ?? "none")", log: Logger.ui)
         return enrichedTrack
     }
